656fba2320e677de7bb59470ecd7ba86b5c2bbc7
Lorenzo Muñoz Berzosa <lorenzoberzosa94@gmail.com>
2022-10-22 12:43:43 +0200

    Add connection to PostgresDB

---
+++ src/config/app.config.ts
@@ -0,0 +1,8 @@
+export const EnvConfiguration = () => ({
+  enviroment: process.env.NODE_ENV || 'dev',
+  dbPassword: process.env.DB_PASSWORD,
+  dbName: process.env.DB_PASSWORD,
+  dbHost: process.env.DB_PASSWORD,
+  dbPort: process.env.DB_PASSWORD || 5432,
+  dbUsername: process.env.DB_PASSWORD,
+});
